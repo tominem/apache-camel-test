7c36c7ca46d010e121289fce48c65da1d3d17209
tominem@hotmail.com <tominem@hotmail.com>
2019-02-10 09:09:22 +0000

    csv test

---
+++ src/main/java/com/tomsys/apachecameltest/model/Position.java
@@ -0,0 +1,15 @@
+package com.tomsys.apachecameltest.model;
+
+public class Position {
+	
+	private String id;
+
+	public String getId() {
+		return id;
+	}
+
+	public void setId(String id) {
+		this.id = id;
+	}
+	
+}
---
+++ src/test/java/com/tomsys/apachecameltest/CSVTest.java
@@ -0,0 +1,57 @@
+package com.tomsys.apachecameltest;
+
+import java.util.List;
+
+import org.apache.camel.Exchange;
+import org.apache.camel.Processor;
+import org.apache.camel.RoutesBuilder;
+import org.apache.camel.builder.PredicateBuilder;
+import org.apache.camel.builder.RouteBuilder;
+import org.apache.camel.component.mock.MockEndpoint;
+import org.apache.camel.dataformat.csv.CsvDataFormat;
+import org.apache.camel.dataformat.csv.CsvRecordConverter;
+import org.apache.camel.test.junit4.CamelTestSupport;
+import org.apache.commons.csv.CSVRecord;
+import org.junit.Test;
+
+import com.tomsys.apachecameltest.model.Position;
+
+
+public class CSVTest extends CamelTestSupport {
+	
+	@Test
+	public void test() throws InterruptedException {
+		MockEndpoint mockEndpoint = getMockEndpoint("mock:result");
+		mockEndpoint.expectedMessageCount(3);
+		
+		assertMockEndpointsSatisfied();
+	}
+	
+	@Override
+	protected RoutesBuilder createRouteBuilder() throws Exception {
+		return new RouteBuilder() {
+            @Override
+            public void configure() throws Exception {
+                
+            	CsvDataFormat csvFormat = new CsvDataFormat();
+            	csvFormat.setDelimiter('|');
+            	csvFormat.setLazyLoad(true);
+            	
+            	from("file://src/test/resources/out?noop=true")
+            		.unmarshal(csvFormat)
+            		.convertBodyTo(List.class)
+            		.split(body())
+            		.validate(PredicateBuilder.and(simple("${body.size} == 2")))
+            		.process(new Processor() {
+						
+						@Override
+						public void process(Exchange exchange) throws Exception {
+							System.out.println("Prop="+ exchange.getProperty("position.id"));
+						}
+					})
+            	.to("mock:result");
+            }
+        };
+	}
+
+}
